210267413c8556feaf2020a7c73216d6eba78b4f
M-Farag <mina.farag@icloud.com>
2023-02-01 23:31:32 +0200

    UPD Simplify code

--- src/main.rs
+++ src/main.rs
@@ -21,13 +21,14 @@ fn main() {
 
     'app_main_loop: loop {
         let dice_1 = Dice::run(String::from("White"));
-        println!("throw_>");
         let mut user_input:String = String::new();
+        
+        println!("throw_>");
+        
         io::stdin().read_line(&mut user_input).expect("Err reading your input");
         
-        let exit_code: char = user_input.chars().take(1).last().unwrap();
-        
-        match exit_code as u8 {
+        let user_input: char = user_input.chars().take(1).last().unwrap();
+        match user_input as u8 {
             88 | 120  => break 'app_main_loop,
             _ => ()
         };
