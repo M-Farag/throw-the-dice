e2c825a5d14cfd78f3616787dad87105cf4484f3
M-Farag <mina.farag@icloud.com>
2023-02-01 23:19:38 +0200

    UPD Simplify the code
    ADD Exit the program on hitting x or X

--- src/main.rs
+++ src/main.rs
@@ -12,28 +12,28 @@ impl Dice {
     {
         Self { color, value: rand::thread_rng().gen_range(1..=6) }
     }
-
-    fn set_color(&mut self, color:String)
-    {
-        self.color = String::from(color);
-    }
-
-    fn change_value(&mut self)
-    {
-        self.value = rand::thread_rng().gen_range(1..=6);
-    }
     
 }
 
 fn main() {
 
-    let mut dice_1 = Dice::run(String::from("White"));
+    
 
     'app_main_loop: loop {
+        let dice_1 = Dice::run(String::from("White"));
         println!("throw_>");
         let mut user_input:String = String::new();
         io::stdin().read_line(&mut user_input).expect("Err reading your input");
-        dice_1.change_value();
+        
+        let exit_code: char = user_input.chars().take(1).last().unwrap();
+        
+        match exit_code as u8 {
+            88 => break 'app_main_loop,
+            120 => break 'app_main_loop,
+            _ => ()
+        };
+        
+
         println!("Dice color: {}, & Value: {}",dice_1.color, dice_1.value);
     }
 }
